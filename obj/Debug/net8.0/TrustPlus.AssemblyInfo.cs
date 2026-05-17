acf42a555fc8ea041865f473c786d642e5bf69b6
shivram1234744 <ay6945565@gmail.com>
2026-05-17 14:52:59 +0530

    constr

--- obj/Debug/net8.0/TrustPlus.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrustPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrustPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0f2549c1ffa2bfa7299fa02afa5e8e5497845f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrustPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrustPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
